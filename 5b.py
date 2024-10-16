4eac7ed714d92b75ee9b0be52b802d4f29e9e7cb
havarsha <varshaha10@gmail.com>
2024-10-16 12:45:19 +0530

    Update 5b.py

--- 5b.py
+++ 5b.py
@@ -3,5 +3,5 @@ CARS=['dester', 'rangerover','swift','scorpio','kia','benz']
 data=[30,40,50,20,60,90]
 plt.figure(figure=(10,7))
 plt.pie(data,labels=CARS)
-plt.title("varsha s 1KI23CS176")
+plt.title("varsha ha 1ki23cs175")
 plt.show()
