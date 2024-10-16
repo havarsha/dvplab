5f189c6d1bc037d2eb35c195f82678c3e073bcd5
havarsha <varshaha10@gmail.com>
2024-10-16 12:46:18 +0530

    Add files via upload

---
+++ lab4a.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+std=['ssclc','puc','1sem','2sem']
+values=[500,600,680,690]
+plt.bar(std,values,color='blue')
+plt.xlabel("Education",fontsize=12)
+plt.ylabel("Marks obtained",fontsize=12)
+plt.title("varsha ha1ki23cs175")
+plt.show()
